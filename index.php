583a4b30ab54b5a3eb9405cbbaab2988a0120b87
diego marchesi <cbdiego.marchesi@gmail.com>
2018-07-20 18:09:22 -0300

    criamos pageadmin e o templet admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -13,6 +14,14 @@ $app->get('/', function() {
     
 	//echo "OK";
 	$page = new Page();
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	//echo "OK";
+	$page = new PageAdmin();
 	$page->setTpl("index");
 
 });
