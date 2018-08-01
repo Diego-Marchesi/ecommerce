5eb81de2bb11a3b291472d1edd64045bb8b62111
diego marchesi <cbdiego.marchesi@gmail.com>
2018-07-31 21:16:03 -0300

    listando produtos dinamicamente no site principal

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ $app->config('debug', true);
 
 require_once("site.php");
 
+require_once("functions.php");
+
 require_once("admin.php");
 
 require_once("admin_user.php");
--- site.php
+++ site.php
@@ -1,12 +1,19 @@
 <?php
 
 use \Hcode\Page;
+use \Hcode\Model\Product;
 
 $app->get('/', function() {
     
+    $products = new Product();
+
+    $products = Product::listAll();
+
 	//echo "OK";
 	$page = new Page();
-	$page->setTpl("index");
+	$page->setTpl("index", array(
+		"products"=>Product::checkList($products)
+	));
 
 });
 
