d83ead6d903c9420653e250672eb3fa9e67e3bd9
diego marchesi <cbdiego.marchesi@gmail.com>
2018-08-05 19:25:55 -0300

    2 parte do carinho de compra

--- site.php
+++ site.php
@@ -77,9 +77,69 @@ $app->get('/cart', function() {
 
 	 $page = new Page();
 
-	$page->setTpl("cart");
+	$page->setTpl("cart", array(
+		"cart"=>$cart->getValues(),
+		"products"=>$cart->getProducts()
+	));
 
 });
+
+// ADICIONANDO ITEM NO CARINHO
+$app->get('/cart/:idproduct/add', function($idproduct) {
+	
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$qtd = (isset($_GET["qtd"]))? (int)$_GET["qtd"] : 1 ;
+
+	for ($i=0; $i < $qtd ; $i++) { 
+		$cart->addProduct($product);
+	}
+
+	
+
+	header("Location: /cart");
+	exit;
+
+});
+
+
+// rmover 1 ITEM NO CARINHO
+$app->get('/cart/:idproduct/minus', function($idproduct) {
+	
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product);
+
+	header("Location: /cart");
+	exit;
+
+});
+
+
+// rmover todos os ITEM NO CARINHO
+$app->get('/cart/:idproduct/remove', function($idproduct) {
+	
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
+
+});
+
 
 
 ?>
