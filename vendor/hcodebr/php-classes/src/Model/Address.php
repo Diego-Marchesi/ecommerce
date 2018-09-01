120bac138a8b3554038a73de2e09e6cf9cd3471b
diego marchesi <cbdiego.marchesi@gmail.com>
2018-09-01 18:32:26 -0300

    ajuste endereço

--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -58,16 +58,17 @@ class Address extends Model
 		$sql = new Sql();
 
 
-		$results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", array(
-			"idaddress"=>$this->getidaddress(),
-			"idperson"=>$this->getidperson(),
-			"desaddress"=>utf8_decode($this->getdesaddress()),
-			"descomplement"=>utf8_decode($this->getdescomplement()),
-			"descity"=>utf8_decode($this->getdescity()),
-			"desstate"=>utf8_decode($this->getdesstate()),
-			"descountry"=>utf8_decode($this->getdescountry()),
-			"deszipcode"=>utf8_decode($this->getdeszipcode()),
-			"desdistrict"=>utf8_decode($this->getdesdistrict())
+		$results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :desnumber, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", array(
+			":idaddress"=>$this->getidaddress(),
+			":idperson"=>$this->getidperson(),
+			":desaddress"=>utf8_decode($this->getdesaddress()),
+			":desnumber"=>$this->getdesnumber(),
+			":descomplement"=>utf8_decode($this->getdescomplement()),
+			":descity"=>utf8_decode($this->getdescity()),
+			":desstate"=>utf8_decode($this->getdesstate()),
+			":descountry"=>utf8_decode($this->getdescountry()),
+			":deszipcode"=>utf8_decode($this->getdeszipcode()),
+			":desdistrict"=>utf8_decode($this->getdesdistrict())
 
 		));
 
